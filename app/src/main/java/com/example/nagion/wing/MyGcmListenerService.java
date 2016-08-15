256985ad742ae7def0dd4c3181b8e19237ddd136
SeoKangYoun <crmsnmercury@gmail.com>
2016-08-15 16:51:00 +0900

    sharedpreference

--- app/src/main/java/com/example/nagion/wing/MyGcmListenerService.java
+++ app/src/main/java/com/example/nagion/wing/MyGcmListenerService.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
